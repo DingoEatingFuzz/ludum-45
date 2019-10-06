ded959bdcd6670d04a95e37fca81a5cdbfca1b02
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-06 12:33:33 -0700

    Add networking contract for the game

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Generated;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+
+public class NetworkManager : NetworkManagerBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void resetLevel(RpcArgs args) {
+
+    }
+
+    public override void sendPath(RpcArgs args) {
+
+    }
+
+    public override void victory(RpcArgs args) {
+
+    }
+
+    public override void setLevel(RpcArgs args) {
+
+    }
+
+    public override void setInkLevel(RpcArgs args) {
+
+    }
+}
