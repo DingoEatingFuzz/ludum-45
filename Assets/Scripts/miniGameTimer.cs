323d0d94340f06adb5fbc48874233ef1d28195c2
Nick <naccuardi@gmail.com>
2019-10-05 17:03:56 -0700

    Timer script to handle changing minigame

---
+++ Assets/Scripts/miniGameTimer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniGameTimer : MonoBehaviour
+{
+    public float miniGameTimeLimit = 5;
+    private void Start()
+    {
+        InvokeRepeating("changeMiniGame",0.0f,miniGameTimeLimit);
+    }
+
+    void changeMiniGame()
+    {
+        //Deal with changing the minigame here.
+        //send the RPC here.
+        Debug.Log("Change Game");
+    }
+}
