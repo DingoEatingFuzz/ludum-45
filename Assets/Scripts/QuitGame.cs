841b56aaf662672e5d21521bf95a66d7c779ff14
Nick <naccuardi@gmail.com>
2019-10-07 15:12:16 -0700

    Wired up the Restart Button.

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+    public OurNetworkManager network;
+
     public void doExitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void resetGame()
+    {
+        network.ResetLevel();
     }
 }
