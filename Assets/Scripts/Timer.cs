c463fb7efeb61bae038877e85a885fc50ca2c84e
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-07 13:09:18 -0700

    Timer prefab

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float beginning;
+    private Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.beginning = Time.time;
+        this.timeText = GameObject.Find("TimeText")?.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.timeText) {
+            this.timeText.text = this.FormattedTime();
+        }
+    }
+
+    void ResetTime() {
+        this.beginning = Time.time;
+    }
+
+    string FormattedTime() {
+        float duration = Time.time - beginning;
+        // var span = new TimeSpan(0, 0, 0, (int)duration);
+        var span = new TimeSpan(0, 0, 0, 10000);
+
+        if (span.Hours > 0) {
+            return $"{span.Hours}:{pad(span.Minutes)}:{pad(span.Seconds)}";
+        }
+        return $"{span.Minutes}:{pad(span.Seconds)}";
+    }
+
+    string pad(int number) {
+        return number < 10 ? $"0{number}" : number.ToString();
+    }
+}
