ea9be3ad7742d0d79f86f3ec58a99a3ab706f40f
Nick <naccuardi@gmail.com>
2019-10-06 20:31:47 -0700

    Added script for detecting a win.

---
+++ Assets/Scripts/EndCollider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCollider : MonoBehaviour
+{
+    //Apply this to the ending item with a collider and rigid body
+    public bool isWin = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        isWin = true;
+    }
+}
