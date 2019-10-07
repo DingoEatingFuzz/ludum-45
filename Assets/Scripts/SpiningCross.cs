73402a079bd47ccd8b09259df7c475728f6a513b
Nick <naccuardi@gmail.com>
2019-10-07 14:17:25 -0700

    Added spinning cross prefab.

---
+++ Assets/Scripts/SpiningCross.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiningCross : MonoBehaviour
+{
+
+    public bool isclockWise = true;
+    public float spinSpeed = 50f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (0,0,isclockWise?-spinSpeed*Time.deltaTime:spinSpeed*Time.deltaTime);
+    }
+}
